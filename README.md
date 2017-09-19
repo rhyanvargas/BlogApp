7f1e62a0d0f237ad01b169b6e8319ee4a6be24ba
Rhyan Vargas <rhyanv1@gmail.com>
2017-09-18 20:38:10 -0400

    add navbar, header and footer partials

---
+++ README.md
@@ -0,0 +1,19 @@
+TECH STACK
+=============
+**Databse: mongoDB**
+- *Object-modeling: mongoose*
+**Server: node.js**
+- *Server Framework: express.js*
+**Templating-engine: EJS **
+**Frontend: HTML + CSS + Javscript**
+- *CSS Framework: SemanticUI*
+
+USING RESTful Routes for CRUD
+=============
+
+name      |     url       |    verb     |   description
+-------------------------------------------------------------------------------
+INDEX     /campgrounds        GET        Display list of campgrounds
+NEW       /campgrounds/new    GET        Shows form to create new campground
+CREATE    /campgrounds        POST       Add campground to Database
+SHOW      /campgrounds/:id    GET        Displays info about one campground
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser')
 ,          app = express();
 
 mongoose.connect('mongodb://localhost/restful_blog_app', {useMongoClient: true});
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -21,18 +21,60 @@ var blogSchema = new mongoose.Schema({
   created: { type: Date, default: Date.now }
 });
 
-var blog = mongoose.model('blog', blogSchema)
+var Blog = mongoose.model('Blog', blogSchema)
 
 
-//  RESTFUL ROUTES
+//===========** RESTFUL ROUTES
+
+// MAIN FEED
+app.get('/', function(req,res){
+  res.redirect('/blogs');
+})
+
+// LIST ALL
+app.get('/blogs', function(req,res){
+  
+  // retrieve all blogs from database
+  Blog.find({}, function(err, blogs){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {blogs: blogs});
+    }
+  });
+});
+
+// CREATE
+app.post('/blogs', function(req,res){
+
+  // Retrieve data from form
+  var title = req.body.title;
+  var imageURL = req.body.imageURL;
+  var body = req.body.body;
+  var newPost = { title: title, image: imageURL, body: body};
+
+  // Create campground and save to database
+  Blog.create(newPost, function(err, newCreated){
+
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/blogs');
+    };
+  });
+});
 
 
+// SHOW FORM
+app.get('/new', function(req,res){
+  res.render('new');
+});
 
 
 
 
 // SERVER
 
-app.listen(3000, function(){
-  console.log('==> SERVER RUNNING....')
+app.listen(8080, function(){
+  console.log('==> SERVER RUNNING: http://localhost:8080/ ...')
 });
