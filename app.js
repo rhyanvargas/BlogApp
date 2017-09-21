8fead508e8e2b23a87ef366f61aa3090a362125c
Rhyan Vargas <rhyanv1@gmail.com>
2017-09-21 13:14:49 -0400

    add semantic UI styles and start to reconfigure data models

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // APP CONFIG
 
 var bodyParser = require('body-parser')
-,     mongoose = require('mongoose')
+,     mongoose = require('mongoose')\
 ,      express = require('express')
 ,          app = express();
 
@@ -15,10 +15,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 // MONGOOSE/MODEL CONFIG
 
 var blogSchema = new mongoose.Schema({
-  title: String,
-  image: String,
-  body: String,
-  created: { type: Date, default: Date.now }
+  blog: {
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+  }
 });
 
 var Blog = mongoose.model('Blog', blogSchema)
@@ -27,13 +29,15 @@ var Blog = mongoose.model('Blog', blogSchema)
 //===========** RESTFUL ROUTES
 
 // MAIN FEED
+
 app.get('/', function(req,res){
   res.redirect('/blogs');
 })
 
 // LIST ALL
+
 app.get('/blogs', function(req,res){
-  
+  var blogs = req.body.blog;
   // retrieve all blogs from database
   Blog.find({}, function(err, blogs){
     if (err) {
@@ -45,20 +49,22 @@ app.get('/blogs', function(req,res){
 });
 
 // CREATE
+
 app.post('/blogs', function(req,res){
 
   // Retrieve data from form
-  var title = req.body.title;
-  var imageURL = req.body.imageURL;
-  var body = req.body.body;
-  var newPost = { title: title, image: imageURL, body: body};
+  // var title = req.body.blog.title;
+  // var image = req.body.blog.image;
+  // var body = req.body.blog.body;
+  // var newPost = { title: title, image: image, body: body};
 
   // Create campground and save to database
-  Blog.create(newPost, function(err, newCreated){
-
+  Blog.create(req.body.blog, function(err, newBlog){
     if (err) {
+      res.render('new');
       console.log(err);
     } else {
+      // redirect to blogs
       res.redirect('/blogs');
     };
   });
@@ -66,7 +72,8 @@ app.post('/blogs', function(req,res){
 
 
 // SHOW FORM
-app.get('/new', function(req,res){
+
+app.get('/blogs/new', function(req,res){
   res.render('new');
 });
 
