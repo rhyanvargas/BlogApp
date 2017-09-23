a22df7e966abc0ad9d9d9b8c98b787e2f85053a2
Rhyan Vargas <rhyanv1@gmail.com>
2017-09-23 16:01:54 -0400

    add blog object to post route and pass data in form as blog[value]

--- app.js
+++ app.js
@@ -1,87 +1,61 @@
+// UTILITES
+var bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    express = require('express'),
+    app = express();
 
 // APP CONFIG
-
-var bodyParser = require('body-parser')
-,     mongoose = require('mongoose')\
-,      express = require('express')
-,          app = express();
-
-mongoose.connect('mongodb://localhost/restful_blog_app', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/blog_app',{useMongoClient: true});
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 
-// MONGOOSE/MODEL CONFIG
-
+// SCHEMA -> MODEL - MONGOOSE
 var blogSchema = new mongoose.Schema({
-  blog: {
     title: String,
     image: String,
     body: String,
     created: { type: Date, default: Date.now }
-  }
 });
 
-var Blog = mongoose.model('Blog', blogSchema)
+var Blog = mongoose.model('Blog', blogSchema);
 
 
-//===========** RESTFUL ROUTES
+//************  ROUTES
 
-// MAIN FEED
-
-app.get('/', function(req,res){
-  res.redirect('/blogs');
-})
-
-// LIST ALL
-
-app.get('/blogs', function(req,res){
-  var blogs = req.body.blog;
-  // retrieve all blogs from database
-  Blog.find({}, function(err, blogs){
-    if (err) {
+// DISPLAY ALL BLOGS
+app.get('/blogs', (req,res) =>{
+  Blog.find({}, (err, posts) =>{
+    if(err){
       console.log(err);
     } else {
-      res.render('index', {blogs: blogs});
-    }
-  });
-});
-
-// CREATE
-
-app.post('/blogs', function(req,res){
-
-  // Retrieve data from form
-  // var title = req.body.blog.title;
-  // var image = req.body.blog.image;
-  // var body = req.body.blog.body;
-  // var newPost = { title: title, image: image, body: body};
-
-  // Create campground and save to database
-  Blog.create(req.body.blog, function(err, newBlog){
-    if (err) {
-      res.render('new');
-      console.log(err);
-    } else {
-      // redirect to blogs
-      res.redirect('/blogs');
+       res.render('index', {posts: posts});
     };
   });
 });
 
 
-// SHOW FORM
+// CREATE
+app.post('/blogs', (req,res)=> {
+  // create a blog object
+  Blog.create(req.body.post, (err,newBlog) =>{
+    if (err) {
+      console.log(err);
+      res.render('new');
+    } else {
+      // redirect to blogs route
+        res.redirect('/blogs');
+    };
+  });
+});
 
-app.get('/blogs/new', function(req,res){
+// SHOW NEW FORM
+app.get('/blogs/new', (req,res) =>{
   res.render('new');
 });
 
-
-
-
 // SERVER
-
-app.listen(8080, function(){
-  console.log('==> SERVER RUNNING: http://localhost:8080/ ...')
+app.listen(3000, function(){
+  console.log('SERVER RUNNING http://localhost:3000...');
 });
